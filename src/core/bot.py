c275379155e8bd2940caf857e42183ca92a626b3
baihu <baihu3210@gmail.com>
2026-08-12 11:07:25 +0000

    Add Bot class implementation with extension loading and lifecycle methods

---
+++ src/core/bot.py
@@ -0,0 +1,26 @@
+import os, discord, traceback
+from discord.ext import commands
+
+class Bot(commands.Bot):
+    def __init__(self):
+        intents = discord.Intents.all()
+        
+        super().__init__(
+            command_prefix = "♡", intents = intents
+        )
+        
+    async def setup_hook(self):
+        pass
+    
+    async def load_extensions(slef):
+        for root, _, files in os.walk("./cogs"):
+            for file in files:
+                if file.endswith(".py"):
+                    rel_path = os.path.relpath(os.path.join(root, file), ".")
+                    extension = rel_path.replace(os.sep, ".")[:-3]
+                    
+                    try: await super().load_extension(extension)
+                    except Exception as e: pass
+                    
+    async def on_ready(slef): pass
+    async def close(slef): await super().close()
